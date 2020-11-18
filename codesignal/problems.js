95d379559fbdd451352d0b44953179d57ca73949
Kelly Hansen <kellyhansen182@gmail.com>
2020-11-18 15:33:16 -0800

    alternatingSums()

---
+++ codesignal/problems.js
@@ -0,0 +1,11 @@
+function alternatingSums(a) {
+    var result = [0, 0];
+    for (var i = 0; i < a.length; i++) {
+        if (i % 2 === 0) {
+            result[0] += a[i];
+        } else {
+            result[1] += a[i];
+        }
+    }
+    return result;
+}
