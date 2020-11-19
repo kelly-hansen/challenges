d255f133703167f2157d29132998c3d3b046e364
Kelly Hansen <kellyhansen182@gmail.com>
2020-11-18 16:45:20 -0800

    areSimilar()

--- codesignal/problems.js
+++ codesignal/problems.js
@@ -22,3 +22,30 @@ function addBorder(picture) {
     picture.push(topAndBot)
     return picture;
 }
+
+function areSimilar(a, b) {
+    var switchCount = 0;
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) {
+            if (switchCount > 0) {
+                return false;
+            }
+            for (var x = i + 1; x < a.length; x++) {
+                var switched = a.slice();
+                var switch1 = switched[i];
+                var switch2 = switched[x];
+                switched[i] = switch2;
+                switched[x] = switch1;
+                if (switched[i] === b[i] && switched[x] === b[x]) {
+                    a = switched.slice();
+                    switchCount++;
+                    break;
+                }
+                if (x === a.length - 1) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
