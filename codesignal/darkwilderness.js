cdbd4c3e3a39069647e7a779dc826a5880c67744
Kelly Hansen <kellyhansen182@gmail.com>
2020-11-20 11:47:50 -0800

    knapsackLight()

--- codesignal/darkwilderness.js
+++ codesignal/darkwilderness.js
@@ -10,3 +10,26 @@ function growingPlant(upSpeed, downSpeed, desiredHeight) {
     height -= downSpeed;
   }
 }
+
+function knapsackLight(value1, weight1, value2, weight2, maxW) {
+  var item1 = [value1, weight1];
+  var item2 = [value2, weight2];
+  if (item1[1] + item2[1] <= maxW) {
+    return item1[0] + item2[0];
+  } else if (item1[1] > maxW && item2[1] > maxW) {
+    return 0;
+  }
+  var mostVal;
+  if (item1[0] > item2[0]) {
+    mostVal = item1;
+    leastVal = item2;
+  } else {
+    mostVal = item2;
+    leastVal = item1;
+  }
+  if (mostVal[1] <= maxW) {
+    return mostVal[0];
+  } else {
+    return leastVal[0];
+  }
+}
