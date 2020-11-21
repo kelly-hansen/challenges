fd4d4845d2cf7a86e3ef39ff455d2b6b86c8dd22
Kelly Hansen <kellyhansen182@gmail.com>
2020-11-20 16:35:58 -0800

    lineEncoding()

--- codesignal/rainbowofclarity.js
+++ codesignal/rainbowofclarity.js
@@ -2,3 +2,27 @@ function isDigit(s) {
   var regex = /\d/;
   return regex.test(s);
 }
+
+function lineEncoding(s) {
+  var div = [];
+  var lastIndex = 0;
+  for (var i = 1; i < s.length; i++) {
+    if (s[i - 1] !== s[i]) {
+      div.push(s.substring(lastIndex, i));
+      lastIndex = i;
+    }
+    if (i === s.length - 1) {
+      div.push(s.substring(lastIndex));
+    }
+  }
+  console.log(div);
+  var result = '';
+  for (var i = 0; i < div.length; i++) {
+    if (div[i].length > 1) {
+      result += '' + div[i].length + div[i][0];
+    } else {
+      result += div[i][0];
+    }
+  }
+  return result;
+}
