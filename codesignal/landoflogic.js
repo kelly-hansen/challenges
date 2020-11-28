b8ea52f749cdc5f19e9188f984f08e7066688f2e
Kelly Hansen <kellyhansen182@gmail.com>
2020-11-27 16:04:36 -0800

    longestWord()

--- codesignal/landoflogic.js
+++ codesignal/landoflogic.js
@@ -1,3 +1,19 @@
+function longestWord(text) {
+  var regex = /[^A-Za-z]/;
+  var words = text.split(regex);
+  var long = 0;
+  var result;
+  for (var i = words.length - 1; i >= 0; i--) {
+    if (regex.test(words[i][0])) {
+      words.splice(i, 1);
+    } else if (words[i].length > long) {
+      long = words[i].length;
+      result = words[i];
+    }
+  }
+  return result;
+}
+
 function validTime(time) {
   let hour = parseInt(time.slice(0, 2));
   let min = parseInt(time.slice(3));
