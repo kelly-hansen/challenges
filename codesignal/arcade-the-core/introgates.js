f6ecdb188dc7b94b5ad9e883ee785a018357c8da
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-20 21:55:27 -0800

    largestNumber()

--- codesignal/arcade-the-core/introgates.js
+++ codesignal/arcade-the-core/introgates.js
@@ -2,3 +2,11 @@ function addTwoDigits(n) {
   const numString = n.toString();
   return parseInt(numString[0]) + parseInt(numString[1])
 }
+
+function largestNumber(n) {
+  let result = '';
+  for (let i = 0; i < n; i++) {
+    result += '9';
+  }
+  return parseInt(result);
+}
