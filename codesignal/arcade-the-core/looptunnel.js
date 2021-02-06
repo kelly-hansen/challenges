d6216386ea2dcf8227bfd79754e09175f9fca98e
Kelly Hansen <kellyhansen182@gmail.com>
2021-02-06 12:36:47 -0800

    magicalWell()

--- codesignal/arcade-the-core/looptunnel.js
+++ codesignal/arcade-the-core/looptunnel.js
@@ -20,3 +20,14 @@ function countSumOfTwoRepresentations2(n, l, r) {
   }
   return count;
 }
+
+function magicalWell(a, b, n) {
+  let result = 0;
+  while (n > 0) {
+    result += a * b;
+    a++;
+    b++;
+    n--;
+  }
+  return result;
+}
