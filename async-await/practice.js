35a5f8ccc0d45151fecf444bff47282337b24429
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-04 19:03:06 -0800

    async await practice

--- async-await/practice.js
+++ async-await/practice.js
@@ -1,5 +1,11 @@
 async function getWebsiteData() {
-  const html = await fetch('https://inlandempire.craigslist.org/');
+  const html = await fetch('https://regres.in/api/users/23', {
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  });
 
-
+  console.log(html.ok);
 }
+
+getWebsiteData();
