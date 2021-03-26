e65cc2540efd34ad849e4afd50e70a191adf577c
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-26 14:27:14 -0700

    properNounCorrection()

--- codesignal/arcade-the-core/bookmarket.js
+++ codesignal/arcade-the-core/bookmarket.js
@@ -1,3 +1,7 @@
 function encloseInBrackets(inputString) {
   return `(${inputString})`;
 }
+
+function properNounCorrection(noun) {
+  return noun[0].toUpperCase() + noun.substring(1).toLowerCase();
+}
