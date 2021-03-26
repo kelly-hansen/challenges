5665b1d9a0cf06bc47737de5445774fc0190af13
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-26 14:32:04 -0700

    isTandemRepeat()

--- codesignal/arcade-the-core/bookmarket.js
+++ codesignal/arcade-the-core/bookmarket.js
@@ -5,3 +5,11 @@ function encloseInBrackets(inputString) {
 function properNounCorrection(noun) {
   return noun[0].toUpperCase() + noun.substring(1).toLowerCase();
 }
+
+function isTandemRepeat(inputString) {
+  if (inputString.length % 2) {
+    return false;
+  }
+  const firstHalf = inputString.substring(inputString.length / 2);
+  return firstHalf + firstHalf === inputString;
+}
