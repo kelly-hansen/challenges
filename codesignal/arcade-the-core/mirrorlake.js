99678e96d083b9a0484509815c7ba639ad2ab5dd
Kelly Hansen <kellyhansen182@gmail.com>
2021-04-15 13:46:03 -0700

    stringsConstruction()

--- codesignal/arcade-the-core/mirrorlake.js
+++ codesignal/arcade-the-core/mirrorlake.js
@@ -1,23 +1,53 @@
-// Timeout
+// With nested for loops (timeout error)
+
+// function stringsConstruction(a, b) {
+//   let arrB = b.split('');
+//   let count = 0;
+
+//   for (let i = 0; i < a.length; i++) {
+//     for (let j = 0; j < arrB.length; j++) {
+//       if (a[i] === arrB[j]) {
+//         arrB.splice(j, 1);
+//         if (i === a.length - 1) {
+//           count++;
+//           i = -1;
+//         }
+//         break;
+//       }
+//       if (j === arrB.length - 1) {
+//         return count;
+//       }
+//     }
+//   }
+//   return count;
+// }
 
 function stringsConstruction(a, b) {
-  let arrB = b.split('');
-  let count = 0;
-
+  const aObj = {};
   for (let i = 0; i < a.length; i++) {
-    for (let j = 0; j < arrB.length; j++) {
-      if (a[i] === arrB[j]) {
-        arrB.splice(j, 1);
-        if (i === a.length - 1) {
-          count++;
-          i = -1;
-        }
-        break;
-      }
-      if (j === arrB.length - 1) {
-        return count;
-      }
+    const char = a[i];
+    if (aObj[char] === undefined) {
+      aObj[char] = 1;
+    } else {
+      aObj[char]++;
     }
   }
-  return count;
+  const bObj = {};
+  for (let i = 0; i < b.length; i++) {
+    const char = b[i];
+    if (bObj[char] === undefined) {
+      bObj[char] = 1;
+    } else {
+      bObj[char]++;
+    }
+  }
+  const charCounts = [];
+  for (const char in aObj) {
+    if (!bObj[char]) {
+      return 0;
+    } else {
+      charCounts.push(Math.floor(bObj[char] / aObj[char]));
+    }
+  }
+  return Math.min(...charCounts);
 }
