adb47fa39a07253594e122b931821b992b739419
Kelly Hansen <kellyhansen182@gmail.com>
2021-05-05 11:27:36 -0700

    Replace use of useFormik() with Formik component

--- learn/src/components/SignupForm.js
+++ learn/src/components/SignupForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useFormik } from "formik"
-import "../styles.css";
+import { Formik } from 'formik'
+import "../styles.css"
 
 const validate = values => {
   const errors = {};
@@ -26,57 +26,57 @@ const validate = values => {
 };
 
 const SignupForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      firstName: '',
-      lastName: '',
-      email: ''
-    },
-    validate,
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="firstName">First Name</label>
-      <input
-        id="firstName"
-        name="firstName"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.firstName}
-      />
-      {formik.touched.firstName && formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: ''
+      }}
+      onSubmit={values => {
+          alert(JSON.stringify(values, null, 2))
+        }
+      }
+    >
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          id="firstName"
+          name="firstName"
+          type="text"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.firstName}
+        />
+        {touched.firstName && errors.firstName ? <div>{errors.firstName}</div> : null}
 
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        id="lastName"
-        name="lastName"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.lastName}
-      />
-      {formik.touched.lastName && formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          id="lastName"
+          name="lastName"
+          type="text"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.lastName}
+        />
+        {touched.lastName && errors.lastName ? <div>{errors.lastName}</div> : null}
 
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
-      />
-      {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.email}
+        />
+        {touched.email && errors.email ? <div>{errors.email}</div> : null}
 
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </Formik>
   );
 };
 
